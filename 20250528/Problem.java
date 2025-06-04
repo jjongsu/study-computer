8d45d9528c9526126c1c49045e752dccf2c4237a
hwangjongsu <hjs1000210002@gmail.com>
2025-06-04 19:23:54 +0900

    20250603 study

---
+++ 20250528/Problem.java
@@ -0,0 +1,8 @@
+
+public class Problem {
+    public static void main(String[] args){
+        int i, hap = 0;
+        for ( i = 0; i <=10; i++, hap+=i);
+        System.out.printf("%d, %d\n", i,hap);   
+    }
+}
